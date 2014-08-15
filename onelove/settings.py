571c6b5e0ff31b151b0cd3661956cf88445c3725
Goran Mekić <meka@lugons.org>
2014-08-15 23:13:41 +0200

    Unhardcode static directory.

--- onelove/settings.py
+++ onelove/settings.py
@@ -81,6 +81,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/vagrant/web/static'
+STATIC_ROOT = '/var/static'
 
 MEDIA_URL = '/media/'
