a9c960862ac28d8b6763c05f399438c95aecdde6
Goran Mekić <meka@lugons.org>
2014-08-26 13:03:38 +0200

    Basic JQuery login in place.
    Refs #6

--- home/views.py
+++ home/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import login, logout as django_logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic import FormView
+
 from fleets.models import Fleet
+import json
 
 
 class HomeView(FormView):
@@ -23,7 +26,9 @@ class HomeView(FormView):
         if 'next' in self.request.GET:
             context['next'] = self.request.GET['next']
         if self.request.user.is_authenticated():
-            context['fleet_list'] = Fleet.objects.filter(user=self.request.user)
+            context['fleet_list'] = Fleet.objects.filter(
+                user=self.request.user
+            )
         return context
 
     def form_valid(self, form):
@@ -31,6 +36,18 @@ class HomeView(FormView):
             self.success_url = form.data['next']
         login(self.request, form.get_user())
         return super(HomeView, self).form_valid(form)
+
+    def JsonResponse(self, context, **response_kwargs):
+        data = json.dumps(context)
+        response_kwargs['content_type'] = 'application/json'
+        return HttpResponse(data, **response_kwargs)
+
+    def form_invalid(self, form):
+        response = super(HomeView, self).form_invalid(form)
+        if self.request.is_ajax():
+            return self.JsonResponse(form.errors, status=403)
+        else:
+            return response
 
 
 def logout(request):
