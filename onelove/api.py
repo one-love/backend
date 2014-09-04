e0952afa2a39a23e8a3a714e49218f30e48dc5aa
Goran Mekić <meka@lugons.org>
2014-09-05 00:18:15 +0200

    Add application and provider document.

--- onelove/api.py
+++ onelove/api.py
@@ -1,9 +1,8 @@
 from django.contrib.auth import get_user_model
 from tastypie.authorization import Authorization
-from tastypie_mongoengine import resources
-from mongoengine import fields
+from tastypie_mongoengine import resources, fields
 
-from .models import Fleet, Application
+from .models import Fleet, Application, Provider
 
 
 class UserResource(resources.MongoEngineResource):
@@ -13,7 +12,31 @@ class UserResource(resources.MongoEngineResource):
         excludes = ['password']
 
 
+class ApplicationResource(resources.MongoEngineResource):
+    class Meta:
+        object_class = Application
+
+
+class ProviderResource(resources.MongoEngineResource):
+    class Meta:
+        object_class = Provider
+
+
 class FleetResource(resources.MongoEngineResource):
+    applications = fields.EmbeddedListField(
+        of='onelove.api.ApplicationResource',
+        attribute='applications',
+        full=True,
+        null=True
+    )
+
+    providers = fields.EmbeddedListField(
+        of='onelove.api.ProviderResource',
+        attribute='providers',
+        full=True,
+        null=True
+    )
+
     class Meta:
         queryset = Fleet.objects.all()
         authorization = Authorization()
