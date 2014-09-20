0eaa24b48a18bf2f3ef39a70e84e0f5669574995
Goran Mekić <meka@lugons.org>
2014-09-20 13:50:24 +0200

    Add models.

--- onelove/models.py
+++ onelove/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.core.mail import send_mail
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.utils import timezone
 from django.utils.http import urlquote
 from django.utils.translation import ugettext_lazy as _
@@ -98,3 +100,49 @@ class User(AbstractBaseUser, PermissionsMixin):
         Sends an email to this User.
         """
         send_mail(subject, message, from_email, [self.email])
+
+
+class Fleet(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    url = models.CharField(max_length=2048, unique=True)
+    slug = models.SlugField(max_length=256)
+    user = models.ForeignKey(get_user_model())
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Fleet, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Application(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    repo = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(max_length=256)
+    fleet = models.ForeignKey(Fleet, related_name='applications')
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Application, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.name
+
+
+class AmazonProvider(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(max_length=256)
+    access_key = models.CharField(max_length=256)
+    security_key = models.CharField(max_length=256)
+    fleet = models.ForeignKey(Fleet, related_name='amazon_providers')
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(AmazonProvider, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.name
