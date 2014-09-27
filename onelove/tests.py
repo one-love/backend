9ffe8b90e0fecddf52f96abb26fbebd9b7f16961
Goran Mekić <meka@lugons.org>
2014-09-27 20:21:32 +0200

    Add coveralls.io support
    Fixes #11.

--- onelove/tests.py
+++ onelove/tests.py
@@ -1,21 +1,26 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 from django.core.urlresolvers import reverse
-from django.test import TestCase, Client
-import json
+from .models import User
 
 
-class APITest(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.client = Client()
-        data = {
-            'username': 'admin@example.com',
-            'password': 'Sekrit',
-        }
-        response = cls.client.post(
-            path=reverse('login'),
-            data=data,
-        )
-        cls.token = json.loads(response.content)['token']
+class APITest(APITestCase):
+    def setUp(self):
+        """
+        Setup token
+        """
+        user = User.objects.get(email='admin@example.com')
+        self.token, created = Token.objects.get_or_create(user=user)
 
-    def test_v1_dummy(self):
-        self.assertEqual(200, 200)
+    def test_v1_get_fleet(self):
+        """
+        Simple GET on ''fleet-list' URL with anonymous and authenticated user
+        """
+        endpoint = 'fleet-list'
+        response = self.client.get(path=reverse(endpoint))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.get(path=reverse(endpoint))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
