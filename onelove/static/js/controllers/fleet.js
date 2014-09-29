fa98f4d11259848dbcc3d65013f6fe297f020f47
Goran Mekić <meka@lugons.org>
2014-09-29 02:49:30 +0200

    Remove static files.

--- onelove/static/js/controllers/fleet.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('OneLove')
-    .controller('FleetController', function($scope, fleetService) {
-    fleetService.get()
-        .then(function(result) {
-            $scope.fleetList = result.data.results;
-        });
-});
