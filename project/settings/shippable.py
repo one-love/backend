1cc904e764711d50a45d943c215763a6d5090a00
Goran Mekić <meka@lugons.org>
2014-10-15 00:43:16 +0200

    Use PostgreSQL in Shippable #48

--- project/settings/shippable.py
+++ project/settings/shippable.py
@@ -1,12 +1,7 @@
 from __future__ import absolute_import
 from .common import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['NAME'] = 'onelove'
 
 INSTALLED_APPS += (
     'django_nose',
