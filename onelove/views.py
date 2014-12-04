c693e0313fb27874b8e4453c46061a51adba2778
Goran Mekić <meka@lugons.org>
2014-12-04 01:48:42 +0100

    Add fake /me #62

--- onelove/views.py
+++ onelove/views.py
@@ -1,13 +1,69 @@
+import os
+import tempfile
+
 from django.http import Http404
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
 from rest_framework.decorators import detail_route
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 
-from . import serializers, tasks
-from . import models
-import tempfile
-import os
+from . import serializers, tasks, models
+
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ApplicationSerializer
+    queryset = serializer_class.Meta.model.objects.all()
+
+
+class FleetViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.FleetSerializer
+
+    def get_queryset(self):
+        queryset = self.serializer_class.Meta.model.objects.filter(
+            group__in=self.request.user.groups.all()
+        )
+        return queryset
+
+    @detail_route(methods=['POST'])
+    def provision(self, request, pk):
+        fleet = models.Fleet.objects.get(pk=pk)
+        for application in fleet.applications.all():
+            hosts_string = ''
+            for provider in fleet.providers.all().select_subclasses():
+                for host in provider.get_hosts():
+                    hosts_string += \
+                        str(host.ip) + \
+                        ' ansible_ssh_user=' + \
+                        provider.ssh_user + '\n'
+                tmp, tmp_path = tempfile.mkstemp()
+                os.write(tmp, hosts_string)
+                os.close(tmp)
+                inventory, inventory_path = tempfile.mkstemp()
+                os.write(inventory, provider.ssh_key)
+                os.close(inventory)
+                config = {
+                    'repo': application.repo,
+                    'inventory': tmp_path,
+                    'playbook': application.playbook,
+                    'private_key_file': inventory_path,
+                }
+                celery_task = tasks.provision.delay(config)
+                db_task = models.Task(
+                    id=celery_task.id,
+                    fleet=fleet,
+                )
+                db_task.save()
+        return Response(
+            {
+                'results': [str(task.id) for task in fleet.tasks.all()]
+            },
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.GroupSerializer
+    queryset = serializer_class.Meta.model.objects.all()
 
 
 class ProviderViewSet(viewsets.ModelViewSet):
@@ -63,57 +119,6 @@ class ProviderViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ApplicationViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.ApplicationSerializer
-    queryset = serializer_class.Meta.model.objects.all()
-
-
-class FleetViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.FleetSerializer
-
-    def get_queryset(self):
-        queryset = self.serializer_class.Meta.model.objects.filter(
-            group__in=self.request.user.groups.all()
-        )
-        return queryset
-
-    @detail_route(methods=['POST'])
-    def provision(self, request, pk):
-        fleet = models.Fleet.objects.get(pk=pk)
-        for application in fleet.applications.all():
-            hosts_string = ''
-            for provider in fleet.providers.all().select_subclasses():
-                for host in provider.get_hosts():
-                    hosts_string += \
-                        str(host.ip) + \
-                        ' ansible_ssh_user=' + \
-                        provider.ssh_user + '\n'
-                tmp, tmp_path = tempfile.mkstemp()
-                os.write(tmp, hosts_string)
-                os.close(tmp)
-                inventory, inventory_path = tempfile.mkstemp()
-                os.write(inventory, provider.ssh_key)
-                os.close(inventory)
-                config = {
-                    'repo': application.repo,
-                    'inventory': tmp_path,
-                    'playbook': application.playbook,
-                    'private_key_file': inventory_path,
-                }
-                celery_task = tasks.provision.delay(config)
-                db_task = models.Task(
-                    id=celery_task.id,
-                    fleet=fleet,
-                )
-                db_task.save()
-        return Response(
-            {
-                'results': [str(task.id) for task in fleet.tasks.all()]
-            },
-            status=status.HTTP_201_CREATED,
-        )
-
-
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserSerializer
     queryset = serializer_class.Meta.model.objects.all()
@@ -126,6 +131,13 @@ class UserViewSet(viewsets.ModelViewSet):
             obj.set_password(obj.password)
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.GroupSerializer
-    queryset = serializer_class.Meta.model.objects.all()
+class MeView(generics.RetrieveAPIView):
+    serializer_class = serializers.MeSerializer
+
+    def get_queryset(self):
+        queryset = self.serializer_class.Meta.model.objects.get(pk=self.request.user.pk)
+        return queryset
+
+    def get_object(self):
+        self.kwargs['pk'] = self.request.user.pk
+        return super(MeView, self).get_object()
