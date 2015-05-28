b822257de1df17a3ab0f473013a94da7e61ff27d
Goran Mekić <meka@lugons.org>
2015-05-28 19:36:40 +0200

    Restart everything on consul value change

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-set -e
-
-
+uwsgi --stop /run/uwsgi.pid
+python /app/manage.py migrate --noinput
+python /app/manage.py loaddata initial
+python /app/manage.py collectstatic --noinput
+uwsgi --ini /app/uwsgi.ini $@
