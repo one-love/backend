8be347a3015d6eb11deb7083e775cfeb289e7f20
Goran Mekić <meka@lugons.org>
2015-05-29 02:16:52 +0200

    Make sure uwsgi is dead before starting new one

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 uwsgi --stop /run/uwsgi.pid
+sleep 1
+killall uwsgi
+sleep 1
+killall -9 uwsgi
 python /app/manage.py migrate --noinput
 python /app/manage.py loaddata initial
 python /app/manage.py collectstatic --noinput
