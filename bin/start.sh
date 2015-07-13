1686e4a030fa3af696cfb0e06facdf1a652f8bcd
Goran Mekić <meka@lugons.org>
2015-07-13 15:53:33 +0200

    Daemonize celery, put uwsgi options in ini file

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export C_FORCE_ROOT=yes
-celery -A manage.celery worker --workdir=/usr/src/app &
-uwsgi -s 0.0.0.0:9000 -w manage:app
+celery -A manage.celery worker --logfile /dev/stdout --detach --workdir=/usr/src/app
+uwsgi --ini /usr/src/app/uwsgi.ini
