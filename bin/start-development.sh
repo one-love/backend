77d89d3397ebcf98e2508bcd90bae50ed13f3ce2
Goran Mekić <meka@lugons.org>
2015-07-15 18:50:12 +0200

    Add celery to dev mode and configure rabbitmq port

--- bin/start-development.sh
+++ bin/start-development.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export C_FORCE_ROOT=yes
 consul-template -once -config /usr/src/app/consul/onelove.conf
+celery -A manage.celery worker --workdir /usr/src/app --loglevel INFO --pidfile /run/celery.pid --autoreload &
+sleep 2
 python manage.py runserver
