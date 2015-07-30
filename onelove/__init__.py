9ccca8e76ad56770fb2e246a6c1e854eb89af8c5
Goran Mekić <meka@lugons.org>
2015-07-30 22:42:16 +0200

    Add user through flask-security

--- onelove/__init__.py
+++ onelove/__init__.py
@@ -1,8 +1,9 @@
 from celery import Celery
+from flask.ext.mail import Mail
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.restful import Api
+from flask.ext.security import Security, MongoEngineUserDatastore
 from flask_restful_swagger import swagger
-from flask.ext.mail import Mail
 
 
 class OneLove(object):
@@ -10,11 +11,13 @@ class OneLove(object):
     celery = Celery('onelove')
     db = MongoEngine()
     mail = Mail()
+    security = Security()
+    user_datastore = None
 
     def __init__(self, app=None):
         self.app = app
-        if app is not None:
-            self.init_app(app)
+        if self.app is not None:
+            self.init_app(self.app)
 
     def init_app(self, app):
         self.app = app
@@ -28,7 +31,21 @@ class OneLove(object):
         OneLove.celery.conf.update(app.config)
         OneLove.celery.set_default()
         OneLove.celery.set_current()
+
         OneLove.mail.init_app(app)
+
         OneLove.db.init_app(app)
+
+        from models import User, Role
+        OneLove.user_datastore = MongoEngineUserDatastore(
+            OneLove.db,
+            User,
+            Role,
+        )
+        OneLove.security.init_app(
+            self.app,
+            OneLove.user_datastore,
+        )
+
         import urls
         urls.init(OneLove.api)
