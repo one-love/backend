911830fc414067191d85b1db9e44adb926e2db71
Goran Mekić <meka@lugons.org>
2015-08-20 17:52:08 +0200

    First ansible task

--- onelove/tasks.py
+++ onelove/tasks.py
@@ -1,10 +1,7 @@
 from celery import current_app
 from models import Task
-
-
-@current_app.task
-def add(x, y):
-    return x + y
+import ansible.playbook
+from ansible import callbacks
 
 
 @current_app.task(bind=True)
@@ -12,7 +9,27 @@ def provision(self, cluster, app):
     task = Task()
     task.celery_id = self.request.id
     task.save()
+    inventory = ansible.inventory.Inventory('inventory')
+    playbook = 'site.yml'
+    stats = callbacks.AggregateStats()
+    playbook_cb = callbacks.PlaybookCallbacks()
+    runner_cb = callbacks.PlaybookRunnerCallbacks(stats)
+
+    pb = ansible.playbook.PlayBook(
+        playbook=playbook,
+        inventory=inventory,
+        callbacks=playbook_cb,
+        runner_callbacks=runner_cb,
+        stats=stats,
+        remote_user='vagrant',
+        remote_pass='vagrant',
+    )
+
+    result = pb.run()
     return {
         'id': str(task.id),
         'celery_id': str(task.celery_id),
     }
+
+
+
