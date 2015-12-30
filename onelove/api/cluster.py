8432741238bbe8741999b226f40ba3068cd5151c
Goran Mekić <meka@lugons.org>
2015-12-30 01:29:24 +0100

    Fuck programming, use os.system

--- onelove/api/cluster.py
+++ onelove/api/cluster.py
@@ -6,7 +6,7 @@ from .namespaces import ns_cluster
 from .fields import cluster_fields as fields
 from .fields import get_cluster_fields as get_fields
 from flask_jwt import current_identity
-from onelove import OneLove
+from .. import current_app
 
 
 parser = api.parser()
@@ -32,14 +32,14 @@ class ClusterListAPI(ProtectedResource):
         cluster_name = args.get('name')
         cluster = Cluster(cluster_name)
 
-        admin_role = OneLove.user_datastore.find_or_create_role(
+        admin_role = current_app.user_datastore.find_or_create_role(
             name='admin_' + cluster_name,
             description="Cluster %s admin" % cluster_name,
             admin=True,
         )
         cluster.roles.append(admin_role)
 
-        user_role = OneLove.user_datastore.find_or_create_role(
+        user_role = current_app.user_datastore.find_or_create_role(
             name='user_' + cluster_name,
             description="Cluster %s users" % cluster_name,
             admin=False,
@@ -49,7 +49,7 @@ class ClusterListAPI(ProtectedResource):
 
         user = User.objects.get(id=current_identity.get_id())
 
-        OneLove.user_datastore.add_role_to_user(user, admin_role)
+        current_app.user_datastore.add_role_to_user(user, admin_role)
         return cluster, 201
 
 
