f73d46080d0bceabdd2ceb8e0b8b830408ad2917
Goran Mekić <meka@lugons.org>
2015-12-31 02:51:54 +0100

    Run celery as solo

--- bin/restart.sh
+++ bin/restart.sh
@@ -6,8 +6,7 @@ if [ -s /run/celery.pid ]; then
     CELERY_PID=$(cat /run/celery.pid)
     kill -SIGHUP $CELERY_PID
 else
-    celery -A manage.celery worker --logfile /var/log/celery.log --detach --workdir /usr/src/app --loglevel INFO --pidfile /run/celery.pid
-    celery -A manage.celery worker --workdir /usr/src/app --loglevel INFO --pidfile /run/celery.pid --autoreload &
+    celery -A manage.celery worker --workdir /usr/src/app --loglevel INFO --pidfile /run/celery.pid --autoreload -P solo &
 fi
 
 if [ -s /run/uwsgi.pid ]; then
