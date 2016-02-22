9c5c2e56dbd1f7b937287f086a29f07f565bd8b8
Goran Mekić <meka@lugons.org>
2016-02-22 02:21:00 +0100

    Adopt to new frontend path

--- onelove/utils.py
+++ onelove/utils.py
@@ -24,6 +24,6 @@ def reload_frontend():
     content = "export const API_URL = '{url}/api/v0';\n".format(url=url)
     my_directory = os.path.dirname(__file__)
     projects_root = os.path.abspath(my_directory + '../../..')
-    frontend_file_path = projects_root + '/frontend/client/src/backend_url.js'
+    frontend_file_path = projects_root + '/frontend/src/backend_url.js'
     with open(frontend_file_path, 'w+') as frontend_file:
         frontend_file.write(content)
