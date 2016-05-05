675134535f2a6571e5a0b76b1432cbf136f583ec
Goran Mekić <meka@tilda.center>
2016-05-05 19:40:59 +0200

    Docker compose

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 export PROJECT_ROOT=$(readlink -f "$(dirname $0)/..")
+export FLASK_CONFIG="dev"
+
 pip install -U -r "${PROJECT_ROOT}/requirements_dev.txt"
 cp "${PROJECT_ROOT}/local_config.dev.py" "${PROJECT_ROOT}/local_config.py"
+
 sleep 5
 ${PROJECT_ROOT}/create_default_admin.py
 ${PROJECT_ROOT}/manage.py runserver
