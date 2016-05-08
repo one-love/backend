ba6d38b055876a563c93f184670206fd9ece33e0
Goran Mekić <meka@tilda.center>
2016-05-08 17:01:27 +0200

    Cleanup

--- bin/dev.sh
+++ bin/dev.sh
@@ -2,7 +2,6 @@
 
 export PROJECT_ROOT=$(readlink -f "$(dirname $0)/..")
 export FLASK_CONFIG="dev"
-export PYTHONUNBUFFERED=1
 
 pip install -U -r "${PROJECT_ROOT}/requirements_dev.txt"
 cp "${PROJECT_ROOT}/local_config.dev.py" "${PROJECT_ROOT}/local_config.py"
