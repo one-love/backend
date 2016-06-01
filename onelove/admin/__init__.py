5f4d417879716813c7ba70be8b3526da83fa93e2
Goran Mekić <meka@tilda.center>
2016-06-01 12:00:02 +0200

    Remove flask admin

--- onelove/admin/__init__.py
+++
@@ -1,8 +0,0 @@
-from .view import UserView, RoleView, ClusterView
-from ..models import User, Cluster, Role
-
-
-def register_admin_views(admin):
-    admin.add_view(UserView(User))
-    admin.add_view(ClusterView(Cluster))
-    admin.add_view(RoleView(Role))
