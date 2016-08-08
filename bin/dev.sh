fae3648b6a3476f08543953bf016cead93444505
Goran Mekić <meka@tilda.center>
2016-08-08 22:59:39 +0200

    Never die

--- bin/dev.sh
+++ bin/dev.sh
@@ -7,5 +7,7 @@ find "${PROJECT_ROOT}" -name '*.pyc' -delete
 pip install -U -r "${PROJECT_ROOT}/requirements_dev.txt"
 cp "${PROJECT_ROOT}/local_config.dev.py" "${PROJECT_ROOT}/local_config.py"
 
-sleep 5
-${PROJECT_ROOT}/backend.py runserver
+while true; do
+    sleep 5
+    ${PROJECT_ROOT}/backend.py runserver
+done
