2f1f282b25cd112fac019562cc118aba90f8eb91
Goran Mekić <meka@tilda.center>
2016-10-27 01:40:19 +0200

    CBSD startup. Consul cleanup

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,9 @@
 
 export PROJECT_ROOT=$(readlink -f "$(dirname $0)/..")
 
+if [ -x /usr/local/bin/cbsd ]; then
+    cbsd jstart mongodb
+fi
 find "${PROJECT_ROOT}" -name '*.pyc' -delete
 pip install -U -r "${PROJECT_ROOT}/requirements_dev.txt"
 cp "${PROJECT_ROOT}/local_config.dev.py" "${PROJECT_ROOT}/local_config.py"
